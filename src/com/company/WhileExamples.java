9683dcb8ce416bdb8788213ad5209d9c3da79221
Serban <serban.g.constantin@gmail.com>
2020-05-22 11:37:44 +0300

    init commit

--- src/com/company/WhileExamples.java
+++ src/com/company/WhileExamples.java
@@ -147,15 +147,16 @@ public class WhileExamples {
         while (n != 1) {
             if (n % 2 == 0) {
                 n = n / 2;
-                System.out.print(n + " ");
 
             } else {
 
                 n = (n * 3) + 1;
-                System.out.print(n + " ");
+
             }
+            System.out.print(n + " ");
 
         }
+
 
     }
 }
