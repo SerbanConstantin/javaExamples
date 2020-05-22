0212d9657f5ca6720053dfa530fd5e25c7d04ed5
Serban <serban.g.constantin@gmail.com>
2020-05-22 21:26:45 +0300

    init commit

--- src/com/company/InputExamples.java
+++ src/com/company/InputExamples.java
@@ -6,8 +6,10 @@ public class InputExamples {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        inverseNumber();
+        sumOfDigits();
 
+        Scanner in = new Scanner(System.in);
 
         String name = in.next();
         int age = in.nextInt();
@@ -16,6 +18,36 @@ public class InputExamples {
         String cuisinePreference = in.next();
 
         System.out.println("The form for " + name + " is completed. We will contact you if we need a chef that cooks " + cuisinePreference + " dishes." );
+
+    }
+    private static void inverseNumber() {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter nr: ");
+        int a = in.nextInt();
+        System.out.print("enter nr2: ");
+        int b = in.nextInt();
+        int c = a;
+        a = b;
+        b = c;
+
+//        a = a + b;
+//        b = a - b;
+//        a = a - b;
+
+        System.out.println(a + " " + b);
+
+    }
+
+    private static void sumOfDigits() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter nr: ");
+        int number = scanner.nextInt();
+        int last_digit = number % 10;
+        int second_digit = (number / 10) % 10;
+        int first_digit = number / 100;
+        System.out.println(first_digit + second_digit + last_digit);
 
     }
 }
