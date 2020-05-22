ba0cb206264f76ec2b2508e8e20e39524c2843d5
Serban <serban.g.constantin@gmail.com>
2020-05-22 11:19:49 +0300

    init commit

--- src/com/company/WhileExamples.java
+++ src/com/company/WhileExamples.java
@@ -11,7 +11,8 @@ public class WhileExamples {
         //alMaxNumDivBy();
         //findLargestNum();
         //sumFirstNumbers();
-        sumElements();
+        //sumElements();
+        collatzConjecture();
     }
 
     private static void printLetters() {
@@ -129,6 +130,34 @@ public class WhileExamples {
         }
         System.out.println(sum);
 
+    }
+
+    private static void collatzConjecture() {
+
+        /*Given natural number n. Generate a sequence of integers, described in the Collatz conjecture:
+        If n is an even number, divide it in half, if it is odd, multiply it by 3 and add 1.
+         Repeat this operation until we get the number 1 as a result.
+         For example, if the number n = 17, then the sequence looks as the following:
+        17 52 26 13 40 20 10 5 16 8 4 2 1
+        Such a sequence will stop at number 1 for any primary natural number n.*/
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.print(n + " ");
+        int i = 0;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                System.out.print(n + " ");
+
+            } else {
+
+                n = (n * 3) + 1;
+                System.out.print(n + " ");
+            }
+
+        }
+       // System.out.print(i);
 
     }
 }
